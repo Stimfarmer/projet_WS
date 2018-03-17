6d79075a1a66bee656d1e8fd9c87c4374f9f0fa3
Stimfarmer <etienne.sapin@insa-cvl.fr>
2018-03-17 12:37:27 +0100

    Starting CSVlike parsing | a test is called when GET /resources/api

--- Projet_web_services/src/fr/CSVlikeHandler.java
+++ Projet_web_services/src/fr/CSVlikeHandler.java
@@ -3,6 +3,10 @@
  */
 package fr;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.StringReader;
+
 /**
  * @author esapin
  *
@@ -12,6 +16,8 @@ public class CSVlikeHandler {
 	private int nb_rows;
 	
 	private int nb_columns;
+	
+	private String columnsName[];
 
 	public int getNb_rows() {
 		return nb_rows;
@@ -29,9 +35,51 @@ public class CSVlikeHandler {
 		this.nb_columns = nb_columns;
 	}
 
-	public CSVlikeHandler(int nb_rows, int nb_columns) {
-		this.nb_rows = nb_rows;
-		this.nb_columns = nb_columns;
+	public String[] getColumnsName() {
+		return columnsName;
 	}
 
+	public void setColumnsName(String columnsName[]) {
+		this.columnsName = columnsName;
+	}
+	
+	public void showColumnsNames()
+	{
+		for(int i = 0;i<this.getColumnsName().length; i++)
+		{
+			System.out.println("[" + (i+1) + "]" + " = " + this.getColumnsName()[i]);
+		}
+	}
+	
+	public void showProperties()
+	{
+		System.out.println("**************************************");
+		System.out.println("** Showing properties of request... **");
+		System.out.println("**************************************");
+		this.showColumnsNames();
+		System.out.println("Columns: " + this.getNb_columns());
+		System.out.println("Rows: " + this.getNb_rows());
+		System.out.println("**************************************");
+		System.out.println("**         Display stopped.         **");
+		System.out.println("**************************************");
+	}
+	
+	public CSVlikeHandler(String plainText) {
+		BufferedReader reader = new BufferedReader(new StringReader(plainText));
+		try {
+			this.columnsName = reader.readLine().split(",");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		this.nb_columns = this.getColumnsName().length;
+		this.nb_rows = 0;
+		try {
+			while(reader.readLine() != null)
+			{
+				this.nb_rows++;
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 }
--- Projet_web_services/src/fr/Main.java
+++ Projet_web_services/src/fr/Main.java
@@ -13,7 +13,12 @@ import javax.ws.rs.*;
 public class Main {
 
 	@GET
-	public String sayPlainTextHello() {
+	public String helloWorld() {
+		CSVlikeHandler test = 
+				new CSVlikeHandler("email,age,org,postal_code\n" + 
+				"jocelyn.jaouen@insa-cvl.fr,21,INSACVL,18000\n" + 
+				"etiennesapin@orange.fr,21,INSACVL,69530");
+		test.showProperties();
 		return "Hello SAPIN";
 	}
 
